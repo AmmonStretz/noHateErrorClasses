f810a21e6c6f2662a5d8d779f5f792e71bd4547a
AmmonStretz <ammon-mino@web.de>
2020-01-26 10:40:50 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
-#TODO: OS check
 pip install jupyterlab
 pip install spacy
 python -m spacy download de_core_news_sm
 pip install template
 pip install matplotlib
 pip install pandas
-#pip install nbimporter
 pip install spellchecker
---
+++ install_pip3.sh
@@ -0,0 +1,7 @@
+pip3 install jupyterlab
+pip3 install spacy
+python3 -m spacy download de_core_news_sm
+pip3 install template
+pip3 install matplotlib
+pip3 install pandas
+pip3 install spellchecker
